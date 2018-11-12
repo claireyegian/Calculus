b9909a1f6710a4e4675fe63407ba3c327f699e46
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-11-12 15:23:03 -0500

    Updated from Brython Server: 11/12/2018 3:23:02 PM

--- FunctionAnalysisProgram.py
+++ FunctionAnalysisProgram.py
@@ -26,12 +26,15 @@ print(SlopeList)
 NumXVals = len(SlopeList)
 print(NumXVals)
 loc = 0
-for slope in SlopeList:
+print(SlopeList[loc])
+print(SlopeList[loc+1])
+while loc <= (NumXVals - 1):
     Quotient = SlopeList[loc]/SlopeList[loc+1]
     if Quotient > 0:
         pass
     elif Quotient <= 0:
         print(Quotient)
+    loc += 1
 
 """x = a + h
 f1 = eval(function)
