f0d64dcd6fcc23fd71ccb383fc52db12a9e326d6
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-11-13 11:50:44 -0500

    Updated from Brython Server: 11/13/2018 11:51:04 AM

--- FunctionAnalysisProgram.py
+++ FunctionAnalysisProgram.py
@@ -2,14 +2,14 @@
 #11/6/18
 #Function Analysis Program
 
-from math import log10
+from math import log10, sin
 
 function = input('Enter a function')
 IntervalBeg = float(input('Enter the begining of the interval'))
 IntervalEnd = float(input('Enter the end of the interval'))
 
 #FIX VOCAB!!!
-tick = float(input('Enter the distance between ticks'))
+step = float(input('Enter the step'))
 
 #Takes symmetric difference quotient to take the derivative at each designated x value
 SlopeList = []
@@ -22,18 +22,46 @@ while a <= IntervalEnd:
     f2 = eval(function)
     SymDif = (f1 - f2)/(2*h)
     SlopeList.append(SymDif)
-    a += tick
+    a += step
 
 #Finds extreme values
 NumXVals = len(SlopeList)
+ExtremaList = []
+print('There is an extreme around x='+str(IntervalBeg))
 loc = 0
 while loc <= (NumXVals - 2):
-    if SlopeList[loc+1] != 0:
+    if SlopeList[loc] < 0:
+        if SlopeList[loc+1] > 0:
+            print('min')
+        elif SlopesList[loc+1] < 0:
+            pass
+        elif SlopeList[loc+1] == 0:
+            pass
+    elif SlopeList[loc] > 0:
+        if SlopeList[loc+1] < 0:
+            print('max')
+        elif SlopesList[loc+1] > 0:
+            pass
+        elif SlopeList[loc+1] == 0:
+            pass
+    elif SlopeList[loc] == 0:
+        if SlopeList[loc+1] > 0:
+            print('min')
+        elif SlopeList[loc+1] < 0:
+            print('max')
+        elif SlopeList[loc+1] == 0:
+            pass
+    """if SlopeList[loc+1] != 0:
         Quotient = SlopeList[loc]/SlopeList[loc+1]
         if Quotient > 0:
             pass
         elif Quotient <= 0:
-            print('There is an extreme around x='+ str(IntervalBeg + (tick*(loc)+tick*(loc+1))/2))
+            Extrema = (IntervalBeg + (step*(loc)+step*(loc+1))/2)
+            ExtremaList.append(Extrema)
+            print('There is an extreme around x='+ str(Extrema))"""
     loc += 1
+print('There is an extreme around x='+str(IntervalEnd))
 
-
+#Finds increasing/decreasing
+if SlopeList[0] > 0:
+    pass
