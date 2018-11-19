ec83f70218d8a317ef9dc5bc307f0070b5ee06c9
claireyegian <31485787+claireyegian@users.noreply.github.com>
2018-11-19 13:04:25 -0500

    Updated from Brython Server: 11/19/2018 1:05:34 PM

--- RAM.py
+++ RAM.py
@@ -1,1 +1,17 @@
+#Claire Yegian
+#11/19/18
+#RAM program - calculates LRAM, RRAM, and MRAM for an interval of a function
 
+function = input('Enter your function: ')
+IntervalBeg = float(input('Enter the begining of your interval: '))
+IntervalEnd = float(input('Enter the end of your interval: '))
+Width = float(input('Enter the width of the rectangles: '))
+
+LRAM = 0
+XVal =  IntervalBeg
+while XVal < IntervalEnd:
+    x = XVal
+    y = eval(function)
+    LRAM += y*Width
+    XVal += Width
+print(LRAM)
